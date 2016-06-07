3ce6d9336ee82236e73d931cfa164c237858bbce
Sudhagar-Babu <sudhagar.it13@bitsathy.ac.in>
2016-06-07 15:27:58 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,10 @@
+a=int(input("enter the number:\n"))
+f=1
+for i in range(1,a+1):
+		if a==0:
+			print("the factorial of the given number is"+f)
+		elif a==1:
+			print("the factorial of the given number is"+f)
+		else:
+	    		f*=i
+print("the factorial of the given number is " +str(f))
